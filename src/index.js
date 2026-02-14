994d2dab1f5ba24f0a1a8179adb6d58aa38c69c5
Ivan <ityshenko@MacBook-Pro-Ivan.local>
2026-02-15 00:46:47 +0200

    Fix: Added styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // <--- ДОБАВИТЬ ЭТУ СТРОКУ
+import App from './App';
+// ...import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
